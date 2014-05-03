57f4a4eb4234f321f5ebb3bc201b9c2963541a3b
Leandro Lovisolo <leandro@leandro.me>
2014-05-03 04:35:44 -0300

    Medición de RTT para cada salto

--- traceroute.py
+++ traceroute.py
@@ -6,23 +6,27 @@
 
 import sys
 from scapy.all import *
+from time import time
 
 def traceroute(hostname):
 	for i in range(1, 64):
 	    pkt = IP(dst=hostname, ttl=i) / UDP(dport=33434)
 	    # Send the packet and get a reply
+	    t0 = time()
 	    reply = sr1(pkt, verbose=0, timeout=1)
+	    t = (time() - t0) * 1000
+	    
 	    if reply is None:
 	        # No reply =(
 	        # break
-	        print "%d hops away: no reply" % i
+	        print "%d hops away:\tno reply" % i
 	    elif reply.type == 3:
 	        # We've reached our destination
 	        print "Done!", reply.src
 	        break
 	    else:
 	        # We're in the middle somewhere
-	        print "%d hops away: " % i, reply.src
+	        print "%d hops away:\t%s\t(%.3f ms)" % (i, reply.src, t)
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
