855a371c10143ca850cba88076e54884f05ec44e
Leandro Lovisolo <leandro@leandro.me>
2014-05-19 10:50:41 -0300

    Calcular correctamente RTTs. Contemplar más de un gateway por TTL

--- traceroute.py
+++ traceroute.py
@@ -1,60 +1,84 @@
 #!/usr/bin/env python2
 # encoding: utf-8
 
-# Código parcialmente robado de:
-# http://jvns.ca/blog/2013/10/31/day-20-scapy-and-traceroute/
-
 import sys
 import socket
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 from scapy.all import *
 from time import time
 from math import sqrt
 
-def help():
-    return "Usage: %s [hostname]\tfor human-friendly output\n" \
-           "       %s [hostname] -m\tfor machine readable output" \
-           % (sys.argv[0], sys.argv[0])
+PACKAGES_PER_TTL = 50
+PACKAGE_TIMEOUT  = 1
+MAX_TTL          = 64
+
+###############################################################################
+# Traceroute                                                                  #
+###############################################################################
+
+def avg_rtt(ans):
+    rtt_total = 0
+    for i in range(0, len(ans)):
+        rtt_total += ans[i][1].time - ans[i][0].sent_time
+    return rtt_total / len(ans) * 1000
+
+def extract_src(ans):
+    src = []
+    for i in range(0, len(ans)):
+        ip = ans[i][1].src
+        if ip not in src: src.append(ip)
+    return src
 
 def tracehop(hostname, ttl):
-    try:        
-        t0 = time()
-        reply = sr1(IP(dst=hostname, ttl=ttl) / UDP(dport=33434),
-                    verbose=0, timeout=1)
-        t = (time() - t0) * 1000
-        return reply, t
+    try:
+        ans, unans = sr(IP(dst=hostname, ttl=ttl) / ICMP() * PACKAGES_PER_TTL,
+                        verbose=0, timeout=PACKAGE_TIMEOUT)
+
+        if len(ans) == 0:
+            src = '*'
+            rtt = '*'
+            destination_reached = False
+        else:
+            src = extract_src(ans)
+            rtt = avg_rtt(ans)
+            destination_reached = True if ans[0][1].type == 0 else False
+
+        return {'hop': ttl, 'src': src, 'rtt': rtt}, destination_reached
+
     except socket.error as e:
         sys.exit(e)
 
 def traceroute(hostname, human):
     hops = []
 
-    for i in range(1, 64):
-        reply, t = tracehop(hostname, i)
-
-        hop = {"hop": i,
-               "src": "*" if reply is None else reply.src,
-               "rtt": -1  if reply is None else t}
+    for i in range(1, MAX_TTL):
+        hop, destination_reached = tracehop(hostname, i)
         hops.append(hop)
 
         if human:
-            msg = "%d hops away:\t" % i
-            if hop["src"] == "*": msg += "no reply"
-            else:                 msg += "%s\t(%0.3f ms)" % (hop["src"], hop["rtt"])
-            print msg
+            if hop['src'] == '*':
+                print '%-3d hops away: no reply' % i
+            else:
+                print '%-3d hops away: %-15s %0.03f ms' % (i, hop['src'][0], hop['rtt'])
+                for i in range(1, len(hop['src'])):
+                    print '               %s' % hop['src'][i]
 
-        if reply is not None and reply.type == 3:
-            if human: print "Destination reached."
+        if destination_reached:
+            if human: print 'Destination reached.'
             break
 
     return hops
 
-def filter_noreply(hops):
-    return filter(lambda x: x["src"] != "*", hops)
+###############################################################################
+# Statistics                                                                  #
+###############################################################################
+
+def exclude_noreply(hops):
+    return filter(lambda x: x['src'] != '*', hops)
 
 def extract_rtts(hops):
-    return map(lambda x: x["rtt"], filter_noreply(hops))
+    return map(lambda x: x['rtt'], exclude_noreply(hops))
 
 def mean(rtts):
     return sum(rtts) / len(rtts)
@@ -67,9 +91,18 @@ def stdev(rtts):
 def zrtt(rtt, mu, sigma):
     return (rtt - mu) / sigma
 
-if __name__ == "__main__":
+###############################################################################
+# Main                                                                        #
+###############################################################################
+
+def help():
+    return 'Usage: %s [hostname]\tfor human-friendly output\n' \
+           '       %s [hostname] -m\tfor machine readable output' \
+           % (sys.argv[0], sys.argv[0])
+
+if __name__ == '__main__':
     if len(sys.argv) < 2 or len(sys.argv) > 3:     sys.exit(help())
-    if len(sys.argv) == 3 and sys.argv[2] != "-m": sys.exit(help())
+    if len(sys.argv) == 3 and sys.argv[2] != '-m': sys.exit(help())
 
     hostname = sys.argv[1]
     human    = len(sys.argv) == 2
@@ -81,14 +114,19 @@ if __name__ == "__main__":
     sigma = stdev(rtts)
 
     if human:
-        print ""
-        print "=================================================="
-        print ""
-        print "Mean: %f" % mean(rtts)
-        print "Standard deviation: %f" % stdev(rtts)
-        print ""
+        print '\nStatistics:\n'
 
-    print "Hop\tIP Address\tRTT\tZRTT"
-    for hop in filter_noreply(hops):
-        print "%d\t%s\t%0.3f\t%0.3f" % \
-            (hop["hop"], hop["src"], hop["rtt"], zrtt(hop["rtt"], mu, sigma))
+        print 'Hop  IP Addresses    RTT      ZRTT'
+        for hop in exclude_noreply(hops):
+            print '%-3d  %-15s %-8.3f %-8.3f' % \
+                (hop['hop'], hop['src'][0], hop['rtt'], zrtt(hop['rtt'], mu, sigma))
+            for i in range(1, len(hop['src'])):
+                print '     %s' % hop['src'][i]
+
+        print ''
+        print 'Mean RTT:       %0.3f ms' % mean(rtts)
+        print 'Std. deviation: %0.3f ms' % stdev(rtts)
+        print ''
+
+    else:
+        print 'Machine readable output not yet implemented.'
