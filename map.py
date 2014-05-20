ac32c49407e47615b5d79179908cf29987c4d0d8
Leandro Lovisolo <leandro@leandro.me>
2014-05-20 10:06:46 -0300

    Formato del mapa

--- map.py
+++ map.py
@@ -5,6 +5,7 @@ import sys
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
+from route import Route
 
 def corners(lons, lats):
     w = max(lons) - min(lons)
@@ -31,49 +32,62 @@ def create_map(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat):
 
     return m
 
-def plot_route(route, m):
-    lastplace = None
-    for place in route:
-        if(lastplace == None): lastplace = place
+def plot_route(gateways, m):
+    prev_gateway = None
+
+    for gateway in gateways:
+        if(prev_gateway == None): prev_gateway = gateway
         else:
-            # draw line between lastplace and place
+            # draw line between prev_gateway and gateway
             xx = []; yy = []
-            for p in [lastplace, place]:
-                x, y = m(p[0], p[1])
+            for gw in [prev_gateway, gateway]:
+                x, y = m(gw.longitude, gw.latitude)
                 xx.append(x)
                 yy.append(y)
             m.plot(xx, yy, linewidth=2, color='b')
 
-            # m.drawgreatcircle(lastplace[0], lastplace[1],
-            #                   place[0], place[1],
+            # m.drawgreatcircle(prev_gateway.longitude, prev_gateway.latitude,
+            #                   gateway.longitude, gateway.latitude,
             #                   linewidth=2,color='b')
 
-            # mark lastplace
-            x, y = m(lastplace[0], lastplace[1])
-            plt.text(x, y, lastplace[2])
+            # mark prev_gateway
+            x, y = m(prev_gateway.longitude, prev_gateway.latitude)
+            # plt.text(x, y, prev_gateway.location)
             m.plot(x, y, 'ro')
 
-            lastplace = place
+            prev_gateway = gateway
         
-        # mark final place
-        x, y = m(place[0], place[1])
-        plt.text(x, y, place[2])
+        # mark final gateway
+        x, y = m(gateway.longitude, gateway.latitude)
+        # plt.text(x, y, gateway.location)
         m.plot(x, y, 'ro')
 
 if __name__ == '__main__':
-    route = []
-    for line in sys.stdin:
-        latitude  = float(line.split(' ')[0])
-        longitude = float(line.split(' ')[1])
-        city      = ' '.join(line.split(' ')[2:])
-        route.append([longitude, latitude, city])
+    plt.rcParams['text.latex.preamble']=[r'\usepackage{lmodern}']
+    plt.rcParams.update({'text.usetex':       True,
+                         'font.size':         10,
+                         'font.family':       'lmodern',
+                         'text.latex.unicode': True} )
 
-    lons = map(lambda x: x[0], route)
-    lats = map(lambda x: x[1], route)
+    fig = plt.figure()
+    fig.set_size_inches(6, 4) 
+
+    route = Route()
+    route.load('/dev/stdin')
+
+    gateways = []
+    for ttl in route.ttls(exclude_noreplies=True):
+        if route[ttl].main_gateway().has_location():
+            gateways.append(route[ttl].main_gateway())
+
+    lons, lats = [], []
+    for gateway in gateways:
+        lons.append(gateway.longitude)
+        lats.append(gateway.latitude)
     llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat = corners(lons, lats)
 
     m = create_map(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat)
     
-    plot_route(route, m)
-    
+    plot_route(gateways, m)
+
     plt.show()
